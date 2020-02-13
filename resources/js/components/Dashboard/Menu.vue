8030f21acdb67d2219b52c4568805fb9c4d8ea48
demyansson <idcadifi@gmail.com>
2020-02-13 22:55:27 +0200

    menu component

---
+++ resources/js/components/Dashboard/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="dashboard-menu">
+        <b-menu>
+            <b-menu-list label="Project">
+                <b-menu-item icon="plus-circle" label="Create"></b-menu-item>
+                <b-menu-item icon="format-list-bulleted" label="List"></b-menu-item>
+            </b-menu-list>
+        </b-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style scoped>
+    .dashboard-menu {
+        padding: 5px;
+    }
+</style>
