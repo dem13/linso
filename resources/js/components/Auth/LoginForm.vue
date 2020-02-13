aaeb99782d825e56b79980160e0ae2a689d55c3e
demyansson <idcadifi@gmail.com>
2020-02-13 22:58:10 +0200

    login form

---
+++ resources/js/components/Auth/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <form action="/login" method="POST">
+        <section>
+            <b-field label="Email">
+                <b-input icon="email" name="email"
+                         placeholder="Email"
+                         type="email">
+                </b-input>
+            </b-field>
+
+            <b-field label="Password">
+                <b-input name="password" password-reveal
+                         type="password">
+                </b-input>
+            </b-field>
+
+            <b-field label="Remember Me">
+                <b-switch false-value="No"
+                          name="remember"
+                          true-value="Yes">
+                </b-switch>
+            </b-field>
+
+            <div class="is-flex login-button">
+                <div>
+                    <b-button native-type="submit" type="is-primary">Login</b-button>
+                </div>
+                <div>
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+        </section>
+
+        <input :value="csrf" name="_token" type="hidden">
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "LoginForm",
+        data: function () {
+            return {
+                csrf: Laravel.csrf
+            };
+        }
+    }
+</script>
+
+<style scoped>
+    .login-button > div {
+        margin-right: 10px;
+    }
+</style>
